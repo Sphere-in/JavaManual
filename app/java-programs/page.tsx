56f1eeae511d20f3834e3da91b1e463a48634a05
sphere <spherein@hotmail.com>
2024-11-10 17:42:01 +0530

    Remove app/java-programs/page.tsx

--- app/java-programs/page.tsx
+++
@@ -1,6 +0,0 @@
-// import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-// import JavaPrograms from "../page"
-// export default function JavaProgramsPage() {
-//   return <JavaPrograms />
-// }
